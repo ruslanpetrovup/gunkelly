c2975b490f7d12956a1e3586b41e7db5c9b712f0
Ruslan Petrov <48686933+ruslanpetrov228@users.noreply.github.com>
2021-10-20 19:36:55 +0300

    mobil-profil

--- src/js/openWindow.js
+++ src/js/openWindow.js
@@ -3,7 +3,26 @@ const initMobileMenu = () => {
   const menuBtn = document.querySelector('.header__menu-mobile-menu-button');
   const closeBtn = document.querySelector('.header__menu-mobile-close-button');
   const searchBtn = document.querySelector('.header__menu-mobile-search-button');
-  const searchEl = document.querySelector('.search')
+  const searchEl = document.querySelector('.search');
+  const filterMenu = document.querySelector('.searchbuilds__block__filter-mobile');
+  const filterBtn = document.querySelector('.searchbuilds-list-mobile-button-filter');
+  const filterCloseBtn = document.querySelector('.searchbuilds__block__filter-toomb-close');
+  const sorte = document.querySelector('.searchbuilds__block__sorte-mobile')
+  const sortCloseBtn = document.querySelector('.searchbuilds__block__sorte-toomb-close');
+  const sorteBtn = document.querySelector('.searchbuilds-list-mobile-button-sort');
+  const filter = () => {
+      filterMenu.classList.add('open');
+  }
+  const closeFilter = () => {
+    filterMenu.classList.remove('open');
+  }
+  const sorteOpen = () => {
+    sorte.classList.add('open');
+  }
+  const closeSorte = () => {
+    sorte.classList.remove('open');
+  }
+
   const search = () => {
     if (searchEl.classList.contains('open')) {
       searchEl.classList.remove('open');
@@ -33,5 +52,9 @@ const initMobileMenu = () => {
   menuBtn.addEventListener('click', openMenu);
   closeBtn.addEventListener('click', closeMenu);
   searchBtn.addEventListener('click', search);
+  filterBtn.addEventListener('click', filter);
+  filterCloseBtn.addEventListener('click', closeFilter);
+  sorteBtn.addEventListener('click',sorteOpen)
+  sortCloseBtn.addEventListener('click', closeSorte)
 };
 export default initMobileMenu;
