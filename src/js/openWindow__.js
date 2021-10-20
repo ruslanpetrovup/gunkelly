cb02418c3a8114919cf848020c9f36909a51b24e
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-10-20 20:22:18 +0300

    adds button to page-add-buld.html

---
+++ src/js/openWindow__.js
@@ -0,0 +1,43 @@
+const initMobileMenu = () => {
+  const menuEl = document.querySelector('.header__menu-mobile');
+  const menuBtn = document.querySelector('.header__menu-mobile-menu-button');
+  const closeBtn = document.querySelector('.header__menu-mobile-close-button');
+
+  // mobile search
+  const searchBtn = document.querySelector(
+    '.header__menu-mobile-search-button',
+  );
+  const searchEl = document.querySelector('.search');
+  const search = () => {
+    if (searchEl.classList.contains('open')) {
+      searchEl.classList.remove('open');
+    } else {
+      searchEl.classList.add('open');
+    }
+  };
+
+  // no-scroll
+  const body = document.body;
+  const html = document.querySelector('html');
+  body.classList.add('no-scroll');
+
+  const toggleNoScroll = () =>
+    html.classList.toggle('no-scroll') & body.classList.toggle('no-scroll');
+
+  // mobile menu
+  const openMenu = () => {
+    menuEl.classList.add('open');
+    toggleNoScroll();
+    closeBtn.addEventListener('click', closeMenu);
+  };
+  const closeMenu = () => {
+    menuEl.classList.remove('open');
+    toggleNoScroll();
+    closeBtn.removeEventListener('click', closeMenu);
+  };
+
+  menuBtn.addEventListener('click', openMenu);
+  closeBtn.addEventListener('click', closeMenu);
+  searchBtn.addEventListener('click', search);
+};
+export default initMobileMenu;
